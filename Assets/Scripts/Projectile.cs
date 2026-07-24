defaa6053392f11e4527dc228ae451d546ac78d8
Marcim2489 <marciohgimenes@hotmail.com.br>
2026-07-24 14:03:35 -0300

    vamos começar a montar

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -17,6 +17,17 @@ public class Projectile : MonoBehaviour
     public void Launch(float speed, Vector2 direcion)
     {
         rigidbody2d.linearVelocity = direcion*speed;
-        Quaternion.LookRotation(transform.position + 10*(Vector3)direcion);
+        
+        transform.right = rigidbody2d.linearVelocity.normalized;
+        // transform.LookAt(transform.position + 10*(Vector3)direcion);
+        // transform.rotation = Quaternion.LookRotation(transform.position + 10*(Vector3)direcion);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
