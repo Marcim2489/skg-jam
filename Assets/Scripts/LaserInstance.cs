90fa7a23f015187b8106c8f19f4cf2eec85c585c
Marcim2489 <marciohgimenes@hotmail.com.br>
2026-07-24 11:27:00 -0300

    ajuste laser sprite

--- Assets/Scripts/LaserInstance.cs
+++ Assets/Scripts/LaserInstance.cs
@@ -7,7 +7,7 @@ public class LaserInstance : MonoBehaviour
 
     public void ManageSize(float size)
     {
-        Vector2 newSize = new Vector2(0.5f, size);
+        Vector2 newSize = new Vector2(0.6252f, size);
         spriteRenderer.size = newSize;
         boxCollider.size = newSize;
         boxCollider.offset = new Vector2(boxCollider.offset.x, -boxCollider.size.y*0.5f);
